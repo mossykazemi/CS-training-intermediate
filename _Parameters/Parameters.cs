f9d8c365b44211b5770e544165e249f57e1bc6ba
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-01 05:37:07 +0330

    Parameters

---
+++ _Parameters/Parameters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Parameters
+{
+    class Parameters
+    {
+        public int a;
+
+        public void passByVal(int n)
+        {
+            n *= 5;
+            //Console.WriteLine(n);
+        }
+        public void passByRef(ref int m)
+        {
+            m *= 5;
+        }
+        public void passByOut(out int c)
+        {
+            c = 10;
+
+        }
+    }
+}
---
+++ _Parameters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Parameters
+{
+    class Program
+    {
+        static private void func1(Parameters x)
+        {
+            x.a = 1000;
+        }
+        static void Main(string[] args)
+        {
+            Parameters p1 = new Parameters();
+            //int x = 10;
+            //p1.passByVal(x);
+            //p1.passByRef(ref x);
+            //Console.WriteLine(x);
+
+
+            int a;
+            p1.passByOut(out a);
+            Console.WriteLine(a);
+
+            Parameters c1 = new Parameters();
+            c1.a = 50;
+            Console.WriteLine(c1.a);
+            func1(c1);
+            Console.WriteLine(c1.a);
+
+
+            Console.ReadKey();
+        }
+    }
+}
