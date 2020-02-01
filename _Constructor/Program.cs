1d14be74619a5127e463a08b19a4d2b217f0fac8
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-01 05:34:10 +0330

    constructor

---
+++ _Constructor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle(30,50);
+            // inja ham mishe meghdar bedim ham nadim chon constructore ma meghdare avalie dade khodesh va age parantez ha khali bashe moshkeli nist
+            // sazande methodi hast ham naam ba kelas ke karesh meghdar dehie avalie kardane
+            Console.WriteLine(r1.GetArea());
+        }
+    }
+}
