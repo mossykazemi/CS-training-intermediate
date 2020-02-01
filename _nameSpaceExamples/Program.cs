7341596a7cba09ece5e358ee8ee62b9df160afe5
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-01 05:35:25 +0330

    nameSpace

---
+++ _nameSpaceExamples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using my=_nameSpaceExamples.myNamespace;
+using you=_nameSpaceExamples.yourNamespace;
+
+//using _nameSpaceExamples.myNamespace;
+//using _nameSpaceExamples.yourNamespace;
+
+// using mikonim ba kalameye my
+
+namespace _nameSpaceExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            my.Rectangle r1 = new my.Rectangle();
+            you.Rectangle r2 = new you.Rectangle();
+            // r1 azayi dare ke r2 nadare
+        }
+    }
+}
