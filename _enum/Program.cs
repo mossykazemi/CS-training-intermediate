727fc92ea010b7d369c3bd4564113d4fdd92e6bc
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-01 05:35:01 +0330

    enum

---
+++ _enum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _enum
+{
+    class Program
+    {
+        enum Directions
+        {
+            South=1,
+            North,
+            East,
+            West
+        }
+        static void Main(string[] args)
+        {
+            //Directions myDirection;
+            //myDirection = Directions.East;
+            Directions myDirection = Directions.East;
+            Console.WriteLine($"Direction:{myDirection}");
+
+            int code = (int)myDirection;
+            Console.WriteLine(code);
+
+            myDirection = (Directions)3;
+            Console.WriteLine(myDirection);
+
+            Console.ReadKey();
+        }
+    }
+}
