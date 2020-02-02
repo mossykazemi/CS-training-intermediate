4bab7b58f44ef7ac49ecc17b892e60ceae009d1b
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-02 10:37:04 +0330

    Constructor

--- _Constructor/Program.cs
+++ _Constructor/Program.cs
@@ -10,9 +10,13 @@ namespace _Constructor
     {
         static void Main(string[] args)
         {
-            Rectangle r1 = new Rectangle(30,50);
-            // inja ham mishe meghdar bedim ham nadim chon constructore ma meghdare avalie dade khodesh va age parantez ha khali bashe moshkeli nist
             // sazande methodi hast ham naam ba kelas ke karesh meghdar dehie avalie kardane
+
+            Rectangle r1 = new Rectangle();
+            // inja ham mishe meghdar bedim ham nadim chon constructore ma meghdare 
+            //avalie dade khodesh va age parantez ha khali bashe moshkeli nist
+            r1.GetLength();
+            r1.GetBreadth();
             Console.WriteLine(r1.GetArea());
         }
     }
