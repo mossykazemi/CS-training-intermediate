ac7dd6391d4e67731cedb19d02dd44e4a86eebd4
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-02 10:16:08 +0330

    params

---
+++ _paramsExample/Parameters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _paramsExample
+{
+    class Parameters
+    {
+        //public double Average(double x, double y)
+        //{
+        //    return (x + y) / 2;
+        //}
+        //public double Average(double x, double y, double c)
+        //{
+        //    return (x + y + c) / 3;
+        //}
+        //public double Average(double x, double y, double c, double z)
+        //{
+        //    return (x + y + c + z) / 4;
+        //}
+
+        public double Average(params double [] numbers)
+        {
+            double sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum / numbers.Length;
+        }
+    }
+}
---
+++ _paramsExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _paramsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parameters c1 = new Parameters();
+            double[] myArray = { 12.5, 13, 14, 18 };
+            Console.WriteLine(c1.Average(myArray));
+
+            Console.WriteLine(c1.Average(5,10,20,5,6));
+
+            Console.ReadKey();
+        }
+    }
+}
