418f8fbe48b35cb25974ad8b8c91c53b7ccb8eee
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-02 10:17:00 +0330

    partial

---
+++ _partialExample/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _partialExample
+{
+    partial class MyClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("I am in method 1");
+        }
+    }
+}
---
+++ _partialExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _partialExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.Method1();
+            obj.Method2();
+            Console.ReadKey();
+        }
+    }
+}
