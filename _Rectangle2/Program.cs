42e1d9f7503151cdf9e074c62658494c6b0ec952
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-06 02:55:43 +0330

    rectangle

---
+++ _Rectangle2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Rectangle2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            Rectangle r2;
+            r2 = r1;
+            r1.SetLength(10);
+            r2.SetBreadth(20);
+            Console.WriteLine(r1.GetArea());
+            Console.ReadKey();
+        }
+    }
+}
