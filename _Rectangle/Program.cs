928f5ea9be8456ab8a732dbf619ca579abb59e1f
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-06 02:55:24 +0330

    rectangle

---
+++ _Rectangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter Length:");
+            //int l = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Length:");
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //Rectangle r1 = new Rectangle();
+            //r1.SetLength(l);
+            //r1.SetBreadth(b);
+            //Console.WriteLine(r1.GetArea());
+
+            Rectangle r1 = new Rectangle();
+            r1.GetBreadth();
+            r1.GetLength();
+            Console.WriteLine(r1.GetArea());
+            Console.ReadKey();
+        }
+    }
+}
