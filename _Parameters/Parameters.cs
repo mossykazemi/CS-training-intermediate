788fe873057db027d6e63bc36827dd9ec583831c
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-05 05:04:44 +0330

    parameters

--- _Parameters/Parameters.cs
+++ _Parameters/Parameters.cs
@@ -10,15 +10,20 @@ namespace _Parameters
     {
         public int a;
 
+        //Pass by value
         public void passByVal(int n)
         {
             n *= 5;
             //Console.WriteLine(n);
         }
+
+        //Pass by refrence
         public void passByRef(ref int m)
         {
             m *= 5;
         }
+
+        //Pass by out
         public void passByOut(out int c)
         {
             c = 10;
