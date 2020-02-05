768908c859729b348dee1f12b3e7825aad441476
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-06 02:55:00 +0330

    rectangle

---
+++ _Recangle3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Recangle3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            r1.SetDiamentions(10, 20);
+            Console.WriteLine(r1.GetArea());
+        }
+    }
+}
---
+++ _Recangle3/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Recangle3
+{
+    class Rectangle
+    {
+        int length;
+        int breadth;
+        public void SetLength(int newValue)
+        {
+            if (newValue < 0)
+            {
+                Console.WriteLine("The number is less than zero!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            length = newValue;
+        }
+        public void SetBreadth(int newValue)
+        {
+            if (newValue < 0)
+            {
+                Console.WriteLine("The number is less than zero!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            breadth = newValue;
+        }
+        public int GetArea()
+        {
+            return 2 * (length + breadth);
+        }
+        public void GetLength()
+        {
+            Console.WriteLine("Enter length");
+            int a = Convert.ToInt32(Console.ReadLine());
+            SetLength(a);
+        }
+        public void GetBreadth()
+        {
+            Console.WriteLine("Enter Breadth");
+            int b = Convert.ToInt32(Console.ReadLine());
+            SetBreadth(b);
+        }
+        public void SetDiamentions(int length, int breadth)
+        {
+            this.length = length;
+            this.breadth = breadth;
+        }
+    }
+}
