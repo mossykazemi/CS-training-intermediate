27387a05aabc9fe64315ea6abd7edca33c191889
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-09 02:33:51 +0330

    AgeAndHeight

---
+++ AgeAndHeight/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeAndHeight
+{
+    class Program
+    {
+        static void showPerson(int age,int height)
+        {
+            Console.WriteLine($"Age is :{age}");
+            Console.WriteLine($"Height is :{height}");
+        }
+        static void Main(string[] args)
+        {
+            //showPerson(22, 170);
+            //showPerson(170, 22);
+
+            showPerson(height: 170, age: 22);
+            Console.ReadKey();
+        }
+    }
+}
