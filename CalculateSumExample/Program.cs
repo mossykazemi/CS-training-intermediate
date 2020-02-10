dbaf61058433a58ab73b68fa9e5a52f7f03581fd
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-11 02:19:25 +0330

    Calcualte sum

---
+++ CalculateSumExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateSumExample
+{
+    class Program
+    {
+        static int calculateSum(int number1,int number2)
+        {
+            return number1 + number2;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number 1:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number 2:");
+            int y = Convert.ToInt32(Console.ReadLine());
+            // inja 2ta arguman be barname nayad bedim dakhele parantez
+            Console.WriteLine(calculateSum(x,y)); 
+            Console.ReadKey();
+        }
+    }
+}
