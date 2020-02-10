79a062e52fc2a94f23e42b55081b964dc3cd6717
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-11 02:20:07 +0330

    ca3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            //rect1.breadth = 20;
+            //rect1.length = 10;
+
+            rect1.SetBreadth(-20);
+            rect1.SetLength(40);
+            Console.WriteLine(rect1.GetArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp3/Rectangle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Rectangle
+    {
+         int length;
+         int breadth;
+        public void SetLength(int newValue)
+        {
+            if (newValue < 0)
+            {
+                Console.WriteLine("The number is less than zero!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            length = newValue;
+        }
+        public void SetBreadth(int newValue)
+        {
+            if (newValue < 0)
+            {
+                Console.WriteLine("The number is less than zero!");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            breadth = newValue;
+        }
+        public int GetArea()
+        {
+            return 2 * (length + breadth);
+        }
+    }
+}
