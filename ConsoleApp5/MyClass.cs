8092cf875c65dc6f55c35de6a77f2afa161edf69
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-12 04:28:28 +0330

    CA5

---
+++ ConsoleApp5/MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class MyClass
+    {
+        public int a;
+        public static int b;
+        public void SetB(int n)
+        {
+            b = n;
+            //baraye inke meghdar az karbar gerefte shavad
+        }
+        public static void ShowB()
+        {
+            Console.WriteLine(b);
+        }
+        //azaye static ham dar method haye mamoli ghabele dastressi hastan ham dar static ha
+        //ama azaye mamoli faghat dar method haye mamoli ghabel dastress hastan
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // static is Shared memory
+            //MyClass.b = 30;
+            MyClass obj1 = new MyClass();
+            obj1.a = 10;
+            MyClass obj2 = new MyClass();
+            obj2.a = 20;
+            MyClass obj3 = new MyClass();
+            obj3.a = 30;
+
+            //MyClass.b = 100;
+            //inja 100 override shod
+
+            obj1.SetB(200);
+            MyClass.ShowB();
+
+            Console.WriteLine($"Value of a :{obj1.a} and Value of b :{MyClass.b}");
+            Console.WriteLine($"Value of a :{obj2.a} and Value of b :{MyClass.b}");
+            Console.WriteLine($"Value of a :{obj3.a} and Value of b :{MyClass.b}");
+
+            Console.ReadKey();
+        }
+    }
+}
