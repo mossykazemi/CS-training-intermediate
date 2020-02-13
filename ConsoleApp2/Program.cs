a33e2829f02c22213657990c380f2917eb09c8ad
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-14 02:32:32 +0330

    Params

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,9 +8,9 @@ namespace _params
 {
     class Program
     {
-        // kalameye params hamishe dar akhare arguman miad
+        // Params keyword always comes at end of arguman
         static int CalculateSum (params int[] numbers)
-        {
+        {//Static method to use params 
             int sum = 0;
             foreach (int num in numbers)
             {
@@ -23,6 +23,8 @@ namespace _params
             Console.WriteLine(CalculateSum(1,2,3,4,5));
             Console.WriteLine(CalculateSum(2,6));
             Console.WriteLine(CalculateSum(7,8,9,5,4,6,4,5));
+
+            //Use result variable
             int result;
             result=CalculateSum(2, 5, 6, 4);
             Console.WriteLine(result);
