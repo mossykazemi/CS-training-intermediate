f5e4572abca338fa5b3aa8661b8da7756dbb6bd2
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-19 03:48:04 +0330

    CopyConstructor

--- CopyConstroctor/Program.cs
+++ CopyConstroctor/Program.cs
@@ -9,7 +9,9 @@ namespace CopyConstroctor
     class Program
     {
         // copy constructor , ye copy az object vorodi ro dar objecti ke dar haale ejrast copy mikone
+        // copy Constructor get's a copy of input object and gives it to the running object
         // be darde object hayi mikhore ke eyne ham hastan
+        // it's useful for objects that are same 
         static void Main(string[] args)
         {
             Rectangle r1 = new Rectangle();
