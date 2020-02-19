68f992c9bfa5c24e93621e12392d35e2b870d854
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-20 01:48:34 +0330

    CalculateSum

---
+++ CS day one/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculateSum
+{
+    class Program
+    {
+        static int Calcsum()
+        {
+            int num1 = 10, num2 = 20;
+            //int sum = num1 + num2;
+            //return sum;
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            var result = Calcsum();
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
