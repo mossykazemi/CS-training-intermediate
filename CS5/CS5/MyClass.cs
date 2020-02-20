01a52be43c64b75094bc55db3c8174ea2eea8ff4
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-21 00:35:24 +0330

    cs5

---
+++ CS5/CS5/MyClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS5
+{
+    class MyClass
+    {
+        public int a;
+        public static int b;
+        public void SetB(int n)
+        {
+            b = n;
+        }
+        public static void ShowB()
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ CS5/CS5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyClass obj1 = new MyClass();
+            obj1.a = 10;
+            MyClass obj2 = new MyClass();
+            obj2.a = 120;
+            MyClass obj3 = new MyClass();
+            obj3.a = 40;
+
+            obj1.SetB(200);
+            MyClass.ShowB();
+
+            //Console.WriteLine($"value of a:{obj1.a} and value of b:{MyClass.b}");
+            //Console.WriteLine($"value of a:{obj2.a} and value of b:{MyClass.b}");
+            //Console.WriteLine($"value of a:{obj3.a} and value of b:{MyClass.b}");
+            Console.ReadKey();
+        }
+    }
+}
