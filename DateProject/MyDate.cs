ce8bb2abeaae57db4a42219bddba9ca468dcc475
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-22 08:26:39 +0330

    Date Project Commited

---
+++ DateProject/MyDate.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateProject
+{
+    class MyDate
+    {
+        private int _day;
+        private int _month;
+        private int _year;
+        private static int[] MonthDays = {0,31,31,31,31,31,31,30,30,30,30,30,29};
+        public int Day
+        {
+            set
+            {
+                if(value>=0 && value <= MonthDays[Month])
+                _day = value;
+                else
+                {
+                    Day = 1;
+                }
+            }
+            get
+            {
+                return _day;
+            }
+        }
+        public int Month
+        {
+            set
+            {
+                if (value >= 0 && value <= 12)
+                    _month = value;
+                else
+                    Month = 1;
+            }
+            get
+            {
+                return _month;
+            }
+        }
+        public int Year
+        {
+            set
+            {
+                if (value >= 1300 /*&& value <= 1500*/)
+                    _year = value;
+                else
+                    Year = 1398;
+            }
+            get
+            {
+                return _year;
+            }
+        }
+        public MyDate(int d=1 ,int m=1, int y=1300)
+        {
+            Month = m;
+            Day = d;
+            Year = y;
+        }
+        public MyDate(MyDate x)
+        {
+            Month = x.Month;
+            Day = x.Day;
+            Year = x.Year;
+        }
+        public override string ToString()
+        {
+            return Year + "/" + (Month<10?"0":"")+ Month + "/"+(Day<10?"0":"") + Day;
+        }
+        private bool EndOfMonth()
+        {
+            if (Day == MonthDays[Month])
+                return true;
+            else
+                return false;
+        }
+        private void Increment()
+        {
+            if(Month==12 && EndOfMonth())
+            {
+                Year++;
+                Month = 1;
+                Day = 1;
+            }
+            else if (EndOfMonth())
+            {
+                Month++;
+                Day = 1;
+            }
+            else
+            {
+                Day++;
+            }
+        }
+        private void Decrement()
+        {
+            if (Month == 12 && EndOfMonth())
+            {
+                Year--;
+                Month = 1;
+                Day = 1;
+            }
+            else if (EndOfMonth())
+            {
+                Month--;
+                Day = 1;
+            }
+            else
+            {
+                Day--;
+            }
+        }
+        public MyDate Add1(int n)
+        {
+            for (int i = 1; i <= n; i++)
+                Increment();
+            return this;
+        }
+        public MyDate Add2(int n)
+        {
+            MyDate temp = new MyDate(this);
+            for (int i = 1; i <= n; i++)
+                temp.Increment();
+            return temp;
+        }
+        public MyDate Remove(int n)
+        {
+            MyDate temp = new MyDate(this);
+            for (int i = 1; i <= n; i++)
+                temp.Decrement();
+            return temp;
+        }
+        public static MyDate operator +(MyDate x,int n)
+        {
+            MyDate temp = new MyDate(x);
+            for (int i = 0; i < n; i++)
+                temp.Increment();
+            return temp;
+        }
+        public static MyDate operator +(int n,MyDate x)
+        {
+            MyDate temp = new MyDate(x);
+            for (int i = 0; i < n; i++)
+                temp.Increment();
+            return temp;
+        }
+        public static bool operator>(MyDate x,MyDate y)
+        {
+            if (x.Year > y.Year || (x.Year == y.Year && x.Month > y.Month) || (x.Year == y.Year && x.Month == y.Month && x.Day > y.Day))
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <(MyDate x, MyDate y)
+        {
+            if (x.Year < y.Year || (x.Year == y.Year && x.Month < y.Month) || (x.Year == y.Year && x.Month == y.Month && x.Day < y.Day))
+                return true;
+            else
+                return false;
+        }
+        public static MyDate operator ++(MyDate x)
+        {
+            MyDate temp = new MyDate(x);
+            temp.Increment();
+            return temp;
+        }
+        public static MyDate operator --(MyDate x)
+        {
+            MyDate temp = new MyDate(x);
+            temp.Decrement();
+            return temp;
+        }
+        public static explicit operator int(MyDate x)
+        {
+            int n = 0;
+            for (int i = 1; i < x.Month; i++)
+                n += MonthDays[i];
+            n += x.Day;
+            return n;
+        }
+        public static implicit operator string(MyDate x)
+        {
+            return x.ToString();
+        }
+        public static bool operator true(MyDate x)
+        {
+            return (x.Day == 1 && x.Month == 1);
+        }
+        public static bool operator false(MyDate x)
+        {
+            return !(x.Day == 1 && x.Month == 1);
+        }
+    }
+}
---
+++ DateProject/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDate d1 = new MyDate(22,7,1398);
+            MyDate d2 = new MyDate();
+            MyDate d3 = new MyDate(1, 6, 1398);
+
+            //d2 = d1.Add1(15);
+            //d2 = d1.Add2(15);
+            //d2 = d1.Remove(365);
+
+
+            //d2 = d1 + 365;
+            //d2 = 7 + d1;
+
+            //Console.WriteLine(d1.ToString());
+            //Console.WriteLine(d2.ToString());
+
+
+            //if (d1 > d3)
+            //    Console.WriteLine("d3 is greater than d1");
+            //else
+            //    Console.WriteLine("d1 is greater than d3");
+
+
+            //d1++;
+            //++d1;
+            //d1--;
+            //--d1;
+            //Console.WriteLine(d1.ToString());
+
+
+            //MyDate d = new MyDate(1,4,1398);
+            //int a;
+            //a =(int) d;
+            //Console.WriteLine($"{a.ToString()}");
+            //string s;
+            //s = d;
+            //Console.WriteLine(s);
+
+            MyDate d = new MyDate(1, 1, 1398);
+            if(d)
+                Console.WriteLine("Happy new Year");
+
+            Console.ReadKey();
+        }
+    }
+}
