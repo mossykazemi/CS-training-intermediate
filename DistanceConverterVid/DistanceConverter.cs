25b84d323db2c19dfd82940a80c0c6fb00ba7f87
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-24 03:32:26 +0330

    Distance Converter Vid

---
+++ DistanceConverterVid/DistanceConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverterVid
+{
+    public static class DistanceConverter
+    {
+        public static double MeterToCentimeter(double m)
+        {
+            return 100 * m;
+        }
+        public static double MeterToMilimeter(double m)
+        {
+            return 1000 * m;
+        }
+        public static double CentimeterToMeter(double m)
+        {
+            return m / 100;
+        }
+        public static double MiliToCentimeter(double mili)
+        {
+            return mili / 10;
+        }
+        //static DistanceConverter()
+        //{
+        //    Console.WriteLine("I am constructor.");
+        //}
+    }
+}
---
+++ DistanceConverterVid/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverterVid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Console.WriteLine("Enter Your Number:");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Select one Converter:");
+            Console.WriteLine("\n [1]MeterToCentimeter. \n [2]MeterToMilimeter. \n [3]CentimeterToMeter. \n [4]MiliToCentimeter");
+            int choise = Convert.ToInt32(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+                    Console.WriteLine(DistanceConverter.MeterToCentimeter(num)); 
+                    break;
+                case 2:
+                    Console.WriteLine(DistanceConverter.MeterToMilimeter(num)); 
+                    break;
+                case 3:
+                    Console.WriteLine(DistanceConverter.CentimeterToMeter(num)); 
+                    break;
+                case 4:
+                    Console.WriteLine(DistanceConverter.MiliToCentimeter(num));
+                    break;
+                default:
+                    Console.WriteLine("please Enter 1 to 4");
+                    goto start;
+                    //break;
+            }
+
+            //double cm;
+            //cm=DistanceConverter.MiliToCentimeter(100);
+            //Console.WriteLine(cm);
+            Console.ReadKey();
+        }
+    }
+}
