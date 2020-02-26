a67e23ca1d2dcfe2a7804cde33f67f4126dccc24
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-27 01:10:32 +0330

    MethodSignature

---
+++ MethodSignature/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSignatureAndOverride
+{
+    class Program
+    {
+        static void ShowMessage(double number)
+        {
+            Console.WriteLine("double version of method is called.");
+        }
+        static void ShowMessage(int number)
+        {
+            Console.WriteLine("integer version of method is called.");
+        }
+        static void Main(string[] args)
+        {
+            ShowMessage(10);
+            ShowMessage(10.5);
+            // emzaye method ro khodesh tashkhis mide ke double hast ya int
+
+            Console.ReadKey();
+        }
+    }
+}
