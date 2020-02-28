ccf4e849ab996a5a40b4cc45ef533185ac4e652c
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-02-28 10:08:47 +0330

    commit optional parameters 2

---
+++ OptionalParameters2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters2
+{
+    class Program
+    {
+        static void method(int a,int b, int c=100,int d = 200)
+        {
+            Console.WriteLine($"a:{a}");
+            Console.WriteLine($"b:{b}");
+            Console.WriteLine($"c:{c}");
+            Console.WriteLine($"d:{d}");
+        }
+        static void Main(string[] args)
+        {
+            method(10, 20,d:500);
+            // baraye meghdar dadan be d az ( : ) estefade kardim va be meghdare c dast nazadim
+            Console.ReadKey();
+        }
+    }
+}
