f156cde663aebcb986de0150e11fc068b0904d7d
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-01 02:46:11 +0330

    OutExample

---
+++ OutExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutExample
+{
+    class Program
+    {
+        static void GetValue(out int number)
+        {
+            number = 10;
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            GetValue(out num);
+            Console.WriteLine($"Number is : {num}");
+
+            Console.ReadKey();
+        }
+    }
+}
