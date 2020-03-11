299dab741787a596cdbdf8d0ef05d6d6ec369435
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-11 17:25:53 +0330

    Person_Constructor

---
+++ Person_Constructor/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Constructor
+{
+    class Person
+    {
+        //private string name;
+        //private string family;
+        //public string Name
+        //{
+        //    set
+        //    {
+        //        name = value;
+        //    }
+        //    get
+        //    {
+        //        return name;
+        //    }
+        //}
+        //public string Family
+        //{
+        //    set
+        //    {
+        //        family = value;
+        //    }
+        //    get
+        //    {
+        //        return family;
+        //    }
+        //}
+
+            //automatic property
+            // Prop Tab Tab
+        public string Name { get; set; }
+        public string Family { get; set; }
+    }
+}
---
+++ Person_Constructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Name = "Mostafa";
+            p1.Family = "Kazemi";
+
+            Console.ReadKey();
+        }
+    }
+}
