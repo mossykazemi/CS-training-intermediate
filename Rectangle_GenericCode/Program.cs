d41ae7b5fcb68de2347063056d29125763ff256d
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-14 01:20:35 +0330

    Rectangle_GenericCode

---
+++ Rectangle_GenericCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_GenericCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1 = new Rectangle();
+            r1.GetValues();
+            bool r = r1.IsSquare;
+            //r1.Length = -12;
+            //r1.Breadth = 15;
+            Console.WriteLine(r1.GetArea()+"\n"+r);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle_GenericCode/Rectangle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_GenericCode
+{
+    class Rectangle
+    {
+        private int length;
+        private int breadth;
+
+        public int Length
+        {
+            set
+            {
+                if (value < 0)
+                {
+                   Console.WriteLine("Error");
+                   Console.ReadKey();
+                   Environment.Exit(0);
+                }
+              
+                length = value;
+            }
+            get
+            {
+                return length;
+            }
+        }
+        public int Breadth
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+                }
+                breadth = value;
+            }
+            get
+            {
+                return breadth;
+            }
+        }
+
+        public void GetValues()
+        {
+            Console.WriteLine("Enter length of rectangle");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter breadth of rectangle");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Length = a;
+            Breadth = b;
+        }
+        public bool IsSquare
+        {
+            get
+            {
+                if (Length == Breadth)
+                    return true;
+                else
+                    return false;
+            }
+        }
+
+        public int GetArea()
+        {
+            return 2 * (length + breadth);
+        }
+    }
+}
