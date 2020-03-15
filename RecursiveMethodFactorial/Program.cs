a544bb8bf89a85caea69b1073852aa9110e19a7b
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-15 12:02:43 +0330

    RecursiveMethodFactorial

---
+++ RecursiveMethodFactorial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveMethodFactorial
+{
+    class Program
+    {
+        static long Factorial(int number)
+        {
+            if (number == 1)
+                return 1;
+            return number * Factorial(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(5));
+
+            // -----------//
+
+            //Console.WriteLine("Enter a Number :");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //long factorial = 1;
+            //for (int i = num; i >= 1 ; i--)
+            //{
+            //    factorial *= i;
+            //}
+            //Console.WriteLine(factorial);
+
+            Console.ReadKey();
+        }
+    }
+}
