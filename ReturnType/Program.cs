2da090275039e3efb6188d6ef0d9d922f8d06606
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-16 05:23:36 +0330

    ReturnType

---
+++ ReturnType/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnType
+{
+    class Program
+    {
+        static void testReturn()
+        {
+            Console.WriteLine("first line of method");
+            Console.WriteLine("second line of method");
+            //return;
+            // code will stop here because of return
+            Console.WriteLine("third line of method");
+            Console.WriteLine("fourth line of method");
+
+
+        }
+        static void Main(string[] args)
+        {
+            testReturn();
+
+            Console.ReadKey();
+
+        }
+    }
+}
