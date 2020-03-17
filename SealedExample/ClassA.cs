d2759e467fb5b8b3bfc4c06fd34cfb46b397faf4
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-17 04:16:51 +0330

    SealedExample

---
+++ SealedExample/ClassA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SealedExample
+{
+    class ClassA
+    {
+        public virtual void func()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    class ClassB:ClassA
+    {
+        sealed public override void func()
+        {
+            Console.WriteLine("Bye");
+        }
+    }
+    class ClassC:ClassB
+    {
+        
+    }
+}
