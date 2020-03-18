0fb9aa15c73231834c1c75c9ecf42dc5a1c5d967
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-18 10:50:13 +0330

    ShowMessageAndNumber

---
+++ ShowMessageAndNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowMessageAndNumber
+{
+    class Program
+    {
+        static void showMessageAndNumber(string message,int number)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine($"number is:{number}");
+        }
+        static void Main(string[] args)
+        {
+            showMessageAndNumber("hello", 1376);
+            //showMessageAndNumber(1376, "hello"); in eshtebahe 
+            Console.ReadKey();
+        }
+    }
+}
