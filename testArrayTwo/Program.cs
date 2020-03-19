5a2f7c3079386ef134be62f6cc7b313201d3a818
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-19 12:53:35 +0330

    TestArray

---
+++ testArrayTwo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testArrayTwo
+{
+    class Program
+    {
+        static void IncrementElements(int[] numbers)
+        {
+            for(int i=0; i<numbers.Length; i++)
+            {
+                numbers[i]++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 50, 80, 100 };
+            IncrementElements(arr);
+            foreach (int a in arr)
+            {
+                Console.WriteLine(a);
+            }
+            // araye khodesh refrence type hast va khod be khod call by ref khahad shod
+            Console.ReadKey();
+        }
+    }
+}
