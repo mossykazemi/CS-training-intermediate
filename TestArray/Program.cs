686b9172db540a3d95286eaa9a8ea83f819812be
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-19 12:52:34 +0330

    TestArray

--- TestArray/Program.cs
+++ TestArray/Program.cs
@@ -10,11 +10,13 @@ namespace TestArray
     {
         static void TestArray(int[] numbers)
         {
+            // print Array
             foreach (int a in numbers)
             {
                 Console.WriteLine(a);
             }
         }
+
         static void Main(string[] args)
         {
             int[] arr = { 10, 20, 30, 50, 80, 100 };
