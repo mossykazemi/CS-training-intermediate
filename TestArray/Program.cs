6acd28eebbc346d39b5c73192702e4d91118867e
Mostafa Kazemi <m.kazemi.512@gmail.com>
2020-03-19 12:51:40 +0330

    TestArray

---
+++ TestArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestArray
+{
+    class Program
+    {
+        static void TestArray(int[] numbers)
+        {
+            foreach (int a in numbers)
+            {
+                Console.WriteLine(a);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 50, 80, 100 };
+            TestArray(arr);
+
+            Console.ReadKey();
+        }
+    }
+}
